7367b2696129f82ff081e9bb6007705dd869e180
sarmirardjouni <samir.ardjouni@gmail.com>
2021-04-29 09:50:09 +0200

    creation de l'array post

--- loveit/src/app/app.module.ts
+++ loveit/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoveComponent } from './love/love.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoveComponent
   ],
   imports: [
     BrowserModule,
